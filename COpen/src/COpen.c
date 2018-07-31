f6c6a721b6c6fa07aa6d6c3f7b2d0b6eaff51f2d
RobertK66 <Robert.climb@rkos.at>
2018-07-31 17:11:47 +0200

    Ein neues C-LpcOpen Projekt dazu....

---
+++ COpen/src/COpen.c
@@ -0,0 +1,68 @@
+/*
+ ===============================================================================
+ Name        : COpen.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+ ===============================================================================
+ */
+
+#if defined (__USE_LPCOPEN)
+#if defined(NO_BOARD_LIB)
+#include "chip.h"
+#else
+#include "board.h"
+#endif
+#endif
+
+#include <cr_section_macros.h>
+
+// TODO: insert other include files here
+
+// TODO: insert other definitions and declarations here
+
+int main(void) {
+
+#if defined (__USE_LPCOPEN)
+	// Read clock settings and update SystemCoreClock variable
+	SystemCoreClockUpdate();
+#if !defined(NO_BOARD_LIB)
+	// Set up and initialize all required blocks and
+	// functions related to the board hardware
+	Board_Init();
+	// Set the LED to the state of "On"
+	Board_LED_Set(0, true);
+#endif
+#endif
+
+	// TODO: insert code here
+
+	// Force the counter to be placed into memory
+	volatile static int i = 0;
+	volatile static int l = 0;
+	// Enter an infinite loop, just incrementing a counter
+	while (1) {
+		i++;
+		if (i % 1000000 == 0) {
+			l++;
+			if (l & 0x01) {
+				Board_LED_Set(0, false);
+			} else {
+				Board_LED_Set(0, true);
+			}
+			if (l & 0x02) {
+				Board_LED_Set(1, false);
+			} else {
+				Board_LED_Set(1, true);
+			}
+			if (l & 0x04) {
+				Board_LED_Set(2, false);
+			} else {
+				Board_LED_Set(2, true);
+			}
+
+		}
+	}
+	return 0;
+}
